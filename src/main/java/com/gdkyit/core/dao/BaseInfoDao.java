ff0957f3be6e3acea8254792c27b32537f4940b9
manley <manley.chung@foxmail.com>
2017-12-05 15:45:39 +0800

    修改基本类

---
+++ src/main/java/com/gdkyit/core/dao/BaseInfoDao.java
@@ -0,0 +1,16 @@
+package com.gdkyit.core.dao;
+
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Map;
+
+@Repository
+public class BaseInfoDao extends BaseDao {
+
+    public List<Map<String,Object>> getDblist() {
+        String sql = "select * from seed_users";
+
+        return this.jdbcTemplate.queryForList(sql);
+    }
+}
