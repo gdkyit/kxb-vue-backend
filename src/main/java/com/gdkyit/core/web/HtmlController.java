5ed57ecfbf27903494e6c8902c5f30f9fa85b839
xiaohei <826970831@qq.com>
2018-01-05 13:58:10 +0800

    1/5

--- src/main/java/com/gdkyit/core/web/HtmlController.java
+++ src/main/java/com/gdkyit/core/web/HtmlController.java
@@ -8,9 +8,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class HtmlController {
 
     @RequestMapping("/example")
-    public String getIndex(ModelMap map) {
+    public String getIndex() {
         // 加入一个属性，用来在模板中读取
-        map.addAttribute("host", "co222m");
         // return模板文件的名称，对应src/main/resources/templates/example.html
         return "example";
     }
--- src/test/java/com/gdkyit/core/CoreApplicationTests.java
+++ src/test/java/com/gdkyit/core/CoreApplicationTests.java
@@ -1,5 +1,9 @@
 package com.gdkyit.core;
 
+import com.gdkyit.core.domain.Data;
+import com.gdkyit.core.domain.DataDTO;
+import com.google.common.reflect.TypeToken;
+import com.google.gson.Gson;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,6 +11,14 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
+import java.net.URL;
+import java.net.URLConnection;
+import java.util.List;
+import java.util.Map;
 
 import static org.hamcrest.collection.IsCollectionWithSize.hasSize;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
@@ -20,17 +32,66 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 public class CoreApplicationTests {
 
     @Autowired
-	private MockMvc mockMvc;
+    private MockMvc mockMvc;
 
     @Test
-	public void contextLoads() {
-	}
+    public void contextLoads() {
+    }
 
-	@Test
-    public void testController() throws Exception{
+    @Test
+    public void testController() throws Exception {
         this.mockMvc.perform(get("/dblist")).andDo(print()).andExpect(status().isOk())
-                .andExpect(jsonPath("$",hasSize(1)));
+                .andExpect(jsonPath("$", hasSize(1)));
 
+    }
+
+    @Test
+    public void getUrl() {
+        String result = "";
+        BufferedReader in = null;
+        try {
+            String urlNameString = "https://sp0.baidu.com/8aQDcjqpAAV3otqbppnN2DJv/api.php?query=2018年1月&resource_id=6018&cb=jQuery1102009526197252905422_1513932542817";
+            URL realUrl = new URL(urlNameString);
+            // 打开和URL之间的连
+            URLConnection connection = realUrl.openConnection();
+            // 设置通用的请求属性
+            connection.setRequestProperty("accept", "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8");
+            connection.setRequestProperty("content-type", "application/json;charset=gbk");
+            connection.setRequestProperty("connection", "Keep-Alive");
+            connection.setRequestProperty("user-agent",
+                    "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1;SV1)");
+            // 建立实际的连接
+            connection.connect();
+            // 获取所有响应头字段
+            Map<String, List<String>> map = connection.getHeaderFields();
+            // 遍历所有的响应头字段
+            for (String key : map.keySet()) {
+                System.out.println(key + "--->" + map.get(key));
+            }
+            // 定义 BufferedReader输入流来读取URL的响应
+            in = new BufferedReader(new InputStreamReader(
+                    connection.getInputStream()));
+            String line;
+            while ((line = in.readLine()) != null) {
+                result += line;
+            }
+        } catch (Exception e) {
+            System.out.println("发送GET请求出现异常！" + e);
+            e.printStackTrace();
+        } finally {
+            // 使用finally块来关闭输入流
+            try {
+                if (in != null) {
+                    in.close();
+                }
+            } catch (Exception e2) {
+                e2.printStackTrace();
+            }
+        }
+        Gson gson = new Gson();
+        Data data = gson.fromJson(result, new TypeToken<DataDTO>() {
+        }.getType());
+        System.out.println(result);
     }
 
 }
